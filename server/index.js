3c50d608621eb664f8e2451d732eb3ea63f83869
Allakazan <brunorizzardi80014@hotmail.com>
2020-04-18 22:21:42 -0300

    Remove useless stuff

--- server/index.js
+++ server/index.js
@@ -10,12 +10,13 @@ const PORT = process.env.PORT || 5000
 const api = require('./routes');
 
 app.use(express.json());
-//app.use('/emoji-js',express.static(path.resolve(__dirname+'/../node_modules/emoji-js/lib')));
-//app.use('/emoji-data',express.static(path.resolve(__dirname+'/../node_modules/emoji-datasource')));
+
 app.use('/api', api);
 app.use('/avatar', express.static(path.join(__dirname, '/storage/avatar')));
 
+// Serve react static files
 app.use(express.static(path.join(__dirname, '/../client/build')))
+
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'))
