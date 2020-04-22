44208d8b70a984b8c4e1855b83f1f13166841e77
Allakazan <brunorizzardi80014@hotmail.com>
2020-04-22 00:22:38 -0300

    User validations

--- client/src/pages/Index/index.js
+++ client/src/pages/Index/index.js
@@ -14,7 +14,9 @@ export default function Index() {
     const [rooms, setRooms] = useState([])
     const [loading, setLoading] = useState(true)
     const [userName, setUserName] = useState('')
-    const [selectedRoom, setSelectedRoom] = useState(0)
+    const [userNameInvalid, setUserNameInvalid] = useState(false)
+    const [selectedRoom, setSelectedRoom] = useState('default')
+    const [selectedRoomInvalid, setSelectedRoomInvalid] = useState(false)
 
     const history = useHistory();
 
@@ -28,6 +30,12 @@ export default function Index() {
 
     async function handleRegister(e) {
         e.preventDefault()
+    
+        setUserNameInvalid(userName === '')
+        setSelectedRoomInvalid(selectedRoom === 'default')
+
+        if (userName === '' || selectedRoom === 'default') return
+
         setLoading(true)
 
         const { token } = (await api.post('auth', { username: userName })).data
@@ -52,13 +60,20 @@ export default function Index() {
                         <p className="title header-primary">Welcome again !</p>
                         <p className="subtitle header-secondary">Agree is a messaging app for heroes. Let's chat !</p>
                         <form onSubmit={handleRegister}>
+                            <label className={userNameInvalid ? 'input-error' : ''}>
+                                USERNAME {userNameInvalid ? <i>- This field is required</i> : ''}
+                            </label>
                             <input 
-                                type="text" className="userName" placeholder="Your Name" autoComplete="off"
+                                type="text" className={userNameInvalid ? 'input-error' : ''}  autoComplete="off" placeholder="Type a username"
                                 value={userName}
                                 onChange={e => setUserName(e.target.value)}
                             />
-                            <select onChange={e => setSelectedRoom(e.target.value)}>
-                                <option value={selectedRoom} defaultValue>Select a server</option>
+                            <label className={selectedRoomInvalid ? 'input-error' : ''}>
+                                SERVER {selectedRoomInvalid ? <i>- Select a server to talk</i> : ''}
+                            </label>
+                            <select className={selectedRoomInvalid ? 'input-error' : ''} 
+                                onChange={e => setSelectedRoom(e.target.value)} >
+                                <option value='default' defaultValue>Select a server</option>
                                 {rooms.map(room => <option key={room.id} value={room.id}>{room.name}</option>)}
                             </select>
                             <button type="submit">Continue</button>
