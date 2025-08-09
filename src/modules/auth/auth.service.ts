ac5f0f6f4172a65cc33236d508c88309cddd3833
Allakazan <brunorizzardi80014@hotmail.com>
2025-08-09 12:14:56 -0300

    feat: User entity, seeding and authentication

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as argon2 from 'argon2';
+
+import { UsersService } from '../users/users.service';
 import { LoggedUser } from './types/loggedUser.type';
 
 @Injectable()
@@ -15,9 +21,11 @@ export class AuthService {
     pass: string,
   ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
+
+    if (!user) throw new UnauthorizedException();
+
+    if (!(await argon2.verify(user.password, pass)))
       throw new UnauthorizedException();
-    }
 
     const payload = { sub: user.id, username: user.username } as LoggedUser;
     return {
